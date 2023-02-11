04e6c1a1d5ac5376f5bde56e533a602aef0969f2
Angel <a_martinez00@outlook.com>
2023-02-11 18:11:19 -0500

    added api

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { NavBar } from './navigation/navbar';
 import { HomePage } from './pages/home-page';
 import { PotlukkDetailsHostPage } from './pages/potlukk-details-host';
 import { RegistrationPage } from './pages/registration-page';
 import { SignInPage } from './pages/sign-in-page';
 
-    // export type Dish = {
-    //     name: string
-    //     description: string
-    //     broughtBy: number //userId
-    //     serves: number
-    //     allergens: []
-    // }
-    
-    // export type PotlukkCreationForm = {
-    //     time: number
-    //     location: string
-    //     desc:string
-    //     isPublic: boolean
-    //     dishes: Dish[]
-    //     attendees: Attendee[]
-    // }
+const queryClient = new QueryClient();
 
 function App() {
   return (<>
   <BrowserRouter>
     <NavBar></NavBar>
+  <QueryClientProvider client={queryClient}>
+
   <Routes>
+
     <Route path='/' element={<SignInPage/>}/>
     <Route path='/home' element={<HomePage/>}/>
     <Route path='/registration' element={<RegistrationPage/>}/>
@@ -36,6 +25,7 @@ function App() {
 
   </Routes>
   
+  </QueryClientProvider>
   </BrowserRouter>
   
   </>
--- src/reducers/home-reducer.tsx
+++ src/reducers/home-reducer.tsx
@@ -1,5 +1,7 @@
 
 
 export type HomeState = {
-    invites = 
+    invites: []
+    potlukks: []
+    notifs: []
 }
